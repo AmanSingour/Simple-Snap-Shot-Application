b0502935bff0176a2dd1a18b4afe0d620c102d49
Aman Singour <amansigour15@gmail.com>
2021-06-14 17:33:12 +0530

    Search component created

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+//? THIS IS SEARCH BAR COMPONENT...
+export const SearchBar = () =>{
+
+    //onChange METHOD TO BE DECLARED HERE
+
+    return(
+        <>
+
+        </>
+    )
+}
+
+export default SearchBar
