f0e4327be22f919e9ed0194dbe5d870beca36cf8
Aman Singour <amansigour15@gmail.com>
2021-06-15 01:50:42 +0530

    Login page completed with form validation

--- src/layouts/LoginPage/LoginPage.js
+++ src/layouts/LoginPage/LoginPage.js
@@ -1,27 +1,94 @@
 import React from "react";
-import { Input, Icon } from "semantic-ui-react";
-import style from "./style.css";
+import { Icon, Button, Form, Header } from "semantic-ui-react";
+import style from "./style.module.css";
 
-//? THIS IS LOGIN PAGE ACCESSABLE BY ALL USERS
+import { validate } from "../../services";
+
+// THIS IS LOGIN PAGE ACCESSABLE BY ALL USERS
 export const LoginPage = () => {
+  // SINGLE STATETO STORE INPUT VALUES
+  const [inputs, setInputs] = React.useState({
+    email: "",
+    password: "",
+  });
+
+  const [errors, setErrors] = React.useState({
+    email: "",
+    password: "",
+  });
+
+  // THIS IS A SINGLE METHOD TO HANDLE INPUT CHANGE
+  const handleChange = (e) => {
+    // SETTING CURRENT INPUT VALUE
+    setInputs({ ...inputs, [e.target.name]: e.target.value });
+
+    setErrors({ ...errors, [e.target.name]: validate(e.target) });
+  };
+
+  // METHOD TO HANDLE FORM SUBMISSION
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // API CALL......
+  };
 
   return (
     <>
       <div className={style.Container}>
-        <div className={style.Form}>
-          <div className={style.InputField}>
-            <Input iconPosition="left" placeholder="Username or Email">
+        {/* I'VE USED SEMANTIC UI COMPONENTS */}
+        <Form className={style.Form}>
+          <Header as="h2">Login</Header>
+          <br />
+          <Form.Field className={style.InputField}>
+            {/* <label>Username</label> */}
+            <Form.Input
+              iconPosition="left"
+              placeholder="Username or Email"
+              type="email"
+              name="email"
+              required
+              error={
+                errors.email !== "" && {
+                  content: errors.email,
+                  pointing: "below",
+                }
+              }
+            >
               <Icon name="at" />
-              <input type="email" name="email" required />
-            </Input>
-          </div>
-          <div className={style.InputField}>
-            <Input iconPosition="left" placeholder="Password">
+              <input required onChange={(e) => handleChange(e)} />
+            </Form.Input>
+          </Form.Field>
+          <Form.Field className={style.InputField}>
+            {/* <label>Password</label> */}
+            <Form.Input
+              iconPosition="left"
+              placeholder="Password"
+              required
+              type="password"
+              name="password"
+              onChange={(e) => handleChange(e)}
+              error={
+                errors.password !== "" && {
+                  content: errors.password,
+                  pointing: "below",
+                }
+              }
+            >
               <Icon name="key" />
-              <input type="password" name="password" required />
-            </Input>
-          </div>
-        </div>
+              <input />
+            </Form.Input>
+          </Form.Field>
+          <Form.Field className={style.Button}>
+            <Button 
+              type="submit" 
+              disabled={
+                Object.values(inputs).includes("") ||
+                Object.values(errors).find((e) => e !== "")
+              }
+              positive onClick={(e) => handleSubmit(e)}>
+              LOGIN
+            </Button>
+          </Form.Field>
+        </Form>
       </div>
     </>
   );
