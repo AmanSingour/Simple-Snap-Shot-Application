9e004ffd0cf13ab89db81a9753331f1fc0197c6e
Aman Singour <amansigour15@gmail.com>
2021-06-14 19:01:31 +0530

    Auth reducer created

---
+++ src/services/reducers/auth.reducer.js
@@ -0,0 +1,29 @@
+import { 
+    LOGIN_SUCCESS, 
+    LOGOUT 
+} from "../../utils/__actionType";
+
+export const authReducer = (
+    state = { loggedIn: false, user: null },
+    action
+  ) => {
+    switch (action.type) {
+      case LOGIN_SUCCESS:
+        return {
+          ...state,
+          user: action.payload,
+          loggedIn: true,
+        };
+  
+      case LOGOUT:
+        return {
+          ...state,
+          user: null,
+          loggedIn: false,
+        };
+  
+      default:
+        return state;
+    }
+  };
+  
