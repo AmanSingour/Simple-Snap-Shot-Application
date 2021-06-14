6dd3bfd02d6a4fdaddd5522f6125940b525233bc
Aman Singour <amansigour15@gmail.com>
2021-06-14 19:02:11 +0530

    Redux Provider added

--- src/index.js
+++ src/index.js
@@ -6,9 +6,14 @@ import App from "./app";
 // IMPORTING SEMANTIC UI MIN CSS
 import "semantic-ui-css/semantic.min.css";
 
+import { store } from "./services";
+import { Provider } from "react-redux";
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
