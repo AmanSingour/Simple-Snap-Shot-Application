2722ae9cb7a30fc87f35b66f5d9f6eeeb39ecb53
Aman Singour <amansigour15@gmail.com>
2021-06-14 17:32:53 +0530

    Home Page created

---
+++ src/layouts/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+//? THIS IS HOME PAGE ONLY ACCESSABLE BY AUTHENTICATED USERS
+export const Home = () =>{
+    return(
+        <>
+
+        </>
+    )
+}
+
+export default Home
