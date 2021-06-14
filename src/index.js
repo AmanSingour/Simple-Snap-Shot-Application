6e72ae09308e589ccb578def35e467934f88f0c8
Aman Singour <amansigour15@gmail.com>
2021-06-14 17:59:05 +0530

    Semantic UI css imported

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './app';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./app";
+
+// IMPORTING SEMANTIC UI MIN CSS
+import "semantic-ui-css/semantic.min.css";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
