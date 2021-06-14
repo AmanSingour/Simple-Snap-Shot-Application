989fc5a1fd40aa289b9cb0c66ea7a9e66f438cca
Aman Singour <amansigour15@gmail.com>
2021-06-14 17:37:38 +0530

    Singup page created

---
+++ src/layouts/SignupPage/SignupPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+//? THIS IS SIGNUP PAGE ACCESSABLE BY ALL USERS
+export const SignupPage = () =>{
+    return(
+        <>
+
+        </>
+    )
+}
+
+export default SignupPage
