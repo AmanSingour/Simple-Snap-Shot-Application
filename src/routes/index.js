646e59420e349a2ca0405949bbe210731040225b
Aman Singour <amansigour15@gmail.com>
2021-06-14 17:18:10 +0530

    Some Imports  done

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,11 @@
 import React from "react";
 import { Switch } from "react-router-dom";
 
+import {_routes} from '../utils'
+
+import PrivateRoute from './PrivateRoute'
+import PublicRoute from './PublicRoute'
+
 //? ALL ROUTES USED IN APP ARE DEFINED HERE...
 const Routes = () => {
   return (
@@ -13,7 +18,8 @@ const Routes = () => {
       {/* {restricted} IS USED TO RESTRICT USER IF ALREADY LOGGED IN */}
       <PublicRoute path={_routes.LOGIN} component={LoginPage} restricted />
       <PublicRoute path={_routes.SIGNUP} component={SignupPage} restricted />
-      
+      <PublicRoute path={_routes.LOGOUT} component={SignupPage}  />
+
     </Switch>
   );
 };
