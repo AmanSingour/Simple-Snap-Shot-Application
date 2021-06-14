518b6dc4c504f31c17b74dd83d6edb654562c5b5
Aman Singour <amansigour15@gmail.com>
2021-06-14 23:18:13 +0530

    Semantic UI Container added

--- src/app/index.js
+++ src/app/index.js
@@ -1,13 +1,16 @@
 import { HashRouter as Router } from "react-router-dom";
+import { Container } from "semantic-ui-react";
 import Routes from "../routes";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Routes />
-      </Router>
+      <Container>
+        <Router>
+          <Routes />
+        </Router>
+      </Container>
     </div>
   );
 }
