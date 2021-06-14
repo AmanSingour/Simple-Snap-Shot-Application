70bec7f67d172e60d5a1dcae50918ba871063b9b
Aman Singour <amansigour15@gmail.com>
2021-06-14 17:59:24 +0530

    Login Form added

--- src/layouts/LoginPage/LoginPage.js
+++ src/layouts/LoginPage/LoginPage.js
@@ -1,12 +1,30 @@
-import React from 'react'
+import React from "react";
+import { Input, Icon } from "semantic-ui-react";
+import style from "./style.css";
 
 //? THIS IS LOGIN PAGE ACCESSABLE BY ALL USERS
-export const LoginPage = () =>{
-    return(
-        <>
-            
-        </>
-    )
-}
+export const LoginPage = () => {
 
-export default LoginPage
+  return (
+    <>
+      <div className={style.Container}>
+        <div className={style.Form}>
+          <div className={style.InputField}>
+            <Input iconPosition="left" placeholder="Username or Email">
+              <Icon name="at" />
+              <input type="email" name="email" required />
+            </Input>
+          </div>
+          <div className={style.InputField}>
+            <Input iconPosition="left" placeholder="Password">
+              <Icon name="key" />
+              <input type="password" name="password" required />
+            </Input>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default LoginPage;
