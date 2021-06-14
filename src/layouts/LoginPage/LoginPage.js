6c176fd7634f148a89a24e98127157696f82446a
Aman Singour <amansigour15@gmail.com>
2021-06-14 17:34:04 +0530

    Login Page created

---
+++ src/layouts/LoginPage/LoginPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+//? THIS IS LOGIN PAGE ACCESSABLE BY ALL USERS
+export const LoginPage = () =>{
+    return(
+        <>
+
+        </>
+    )
+}
+
+export default LoginPage
