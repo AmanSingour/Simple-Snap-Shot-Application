5b2e538595bf6af63def70eaa35c254c1895489a
Aman Singour <amansigour15@gmail.com>
2021-06-14 18:21:21 +0530

    Router configured

--- src/app/index.js
+++ src/app/index.js
@@ -1,9 +1,13 @@
-import './App.css';
+import { HashRouter as Router } from "react-router-dom";
+import Routes from "../routes";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-        
+      <Router>
+        <Routes />
+      </Router>
     </div>
   );
 }
