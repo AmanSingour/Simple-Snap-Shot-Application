f015692599c55e12d235989a76652475063208e1
Aman Singour <amansigour15@gmail.com>
2021-06-15 01:07:11 +0530

    Regex fixes

--- src/utils/data/_regex.js
+++ src/utils/data/_regex.js
@@ -3,6 +3,6 @@
 export const mailformat =
   /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-export const pwdRegex = /^(?=\\S+$).{4, 20}$/;
+export const pwdRegex = /^[a-zA-Z0-9]{6,}$/
 
-export const nameRegex = /^[a-zA-Z]$/;
+export const nameRegex = /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/
