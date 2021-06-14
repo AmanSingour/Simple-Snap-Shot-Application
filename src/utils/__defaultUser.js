384ac81f30991134842db68ff6c88952c6ceeb01
Aman Singour <amansigour15@gmail.com>
2021-06-14 17:17:37 +0530

    Default users added

---
+++ src/utils/__defaultUser.js
@@ -0,0 +1,8 @@
+const _defaultUser = [
+  { username: "venkat@email.com", password: "12345678" },
+  { username: "akash@email.com", password: "12345678" },
+  { username: "raushan@email.com", password: "12345678" },
+  { username: "mohit@email.com", password: "12345678" },
+];
+
+export default _defaultUser;
