737ef95da949fecb865059d22097077812981150
Aman Singour <amansigour15@gmail.com>
2021-06-15 18:42:37 +0530

    Context provider added

--- src/app/index.js
+++ src/app/index.js
@@ -1,15 +1,33 @@
+import React from "react";
 import { HashRouter as Router } from "react-router-dom";
 import { Container } from "semantic-ui-react";
 import Routes from "../routes";
 import "./App.css";
 
+import { LOGIN_FAILD, LOGIN_SUCCESS } from "../utils/data/__actionType";
+import { useDispatch } from "react-redux";
+import {AuthProvider} from "../services/context";
+
 function App() {
+  const dispatch = useDispatch();
+
+  React.useEffect(() => {
+    let user = null;
+    try {
+      user = JSON.parse(sessionStorage.getItem("user"));
+      if (user) dispatch({ type: LOGIN_SUCCESS, payload: user });
+    } catch (e) {
+      dispatch({ type: LOGIN_FAILD });
+    }
+  }, []);
   return (
     <div className="App">
       <Container>
-        <Router>
-          <Routes />
-        </Router>
+        <AuthProvider>
+          <Router>
+            <Routes />
+          </Router>
+        </AuthProvider>
       </Container>
     </div>
   );
