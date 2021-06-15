2b6650a886a246444961ebb670fad6841896697c
Aman Singour <amansigour15@gmail.com>
2021-06-15 22:36:04 +0530

    Image showcase created

--- src/components/ImageView/ImageView.js
+++ src/components/ImageView/ImageView.js
@@ -1,12 +1,36 @@
-import React from 'react'
+import React from "react";
+import StackGrid, { easings, transitions } from "react-stack-grid";
+import ScrollContainer from "react-indiana-drag-scroll";
+import { Container, Grid, Image } from "semantic-ui-react";
+
+// COMPOSING IMAGE URL
+const imgUrl = ({ farm, server, id, secret }) =>
+  `https://farm${farm}.staticflickr.com/${server}/${id}_${secret}_m.jpg`;
 
 //? THIS IS IMAGE VIEW COMPONENT...
-export const ImageView = data =>{
-    return(
-        <>
+export const ImageView = ({ data }) => {
 
-        </>
-    )
-}
+React.useEffect(() => {
+    
+}, [data])
 
-export default ImageView
+  const Images = data.map((e, index) => (
+    <Grid.Column  key={index}>
+      <Image src={imgUrl(e)} alt="" size='large' />
+    </Grid.Column>
+  ));
+
+  return (
+    <>
+      <div >
+        {data && (
+          <StackGrid >
+              {Images}
+          </StackGrid>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default ImageView;
