e4be1742ccf2051036821a3a3b9a3be55b8cdad9
Aman Singour <amansigour15@gmail.com>
2021-06-15 22:34:39 +0530

    Search faild type added

--- src/utils/data/__actionType.js
+++ src/utils/data/__actionType.js
@@ -3,3 +3,6 @@ export const LOGIN_FAILD = 'LOGIN_FAILD'
 export const SIGNUP_SUCCESS = 'SIGNUP_SUCCESS'
 export const SIGNUP_FAILD = 'SIGNUP_FAILD'
 export const LOGOUT = 'LOGOUT'
+
+export const SEARCH_SUCCESS = 'SEARCH_SUCCESS'
+export const SEARCH_FAILD = 'SEARCH_FAILD'
