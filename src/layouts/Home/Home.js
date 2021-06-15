9240df4a59bc2db741f8bd66d71382ab86033748
Aman Singour <amansigour15@gmail.com>
2021-06-15 18:43:21 +0530

    Context consumer added

--- src/layouts/Home/Home.js
+++ src/layouts/Home/Home.js
@@ -1,12 +1,24 @@
-import React from 'react'
+import React from "react";
+import { AuthContext } from "../../services/context";
+
+import { ImageView, SearchBar } from "../../components";
+
+import style from "./style.module.css";
 
 //? THIS IS HOME PAGE ONLY ACCESSABLE BY AUTHENTICATED USERS
-export const Home = () =>{
-    return(
-        <>
+export const Home = () => {
+  return (
+    <>
+      <div className={style.Container}>
+        <AuthContext.Consumer>
+          {(user) => <h1>Welcome {user.username} to Snap-Shot!</h1>}
+        </AuthContext.Consumer>
 
-        </>
-    )
-}
+        <SearchBar />
+        <ImageView />
+      </div>
+    </>
+  );
+};
 
-export default Home
+export default Home;
