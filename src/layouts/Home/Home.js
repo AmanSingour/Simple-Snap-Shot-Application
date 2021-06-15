e828afd29f6e21e11226cdc9705c90f1f55c21ec
Aman Singour <amansigour15@gmail.com>
2021-06-15 22:34:02 +0530

    Response data passed

--- src/layouts/Home/Home.js
+++ src/layouts/Home/Home.js
@@ -4,18 +4,22 @@ import { AuthContext } from "../../services/context";
 import { ImageView, SearchBar } from "../../components";
 
 import style from "./style.module.css";
+import { useSelector } from "react-redux";
 
 //? THIS IS HOME PAGE ONLY ACCESSABLE BY AUTHENTICATED USERS
 export const Home = () => {
+
+  const photos = useSelector(state => state.photos)
+
   return (
     <>
       <div className={style.Container}>
         <AuthContext.Consumer>
-          {(user) => <h1>Welcome {user.username} to Snap-Shot!</h1>}
+          {(user) => <h1 className={style.Heading}>Welcome {user.username} to Snap-Shot!</h1>}
         </AuthContext.Consumer>
 
         <SearchBar />
-        <ImageView />
+        <ImageView data={photos} />
       </div>
     </>
   );
