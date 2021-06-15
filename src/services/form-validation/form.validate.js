994e43d4f9a72ba9e243ea0327a064db941b4248
Aman Singour <amansigour15@gmail.com>
2021-06-15 12:58:09 +0530

    Logic fix

--- src/services/form-validation/form.validate.js
+++ src/services/form-validation/form.validate.js
@@ -2,7 +2,7 @@
 const validate = (inputs) => {
   return (errors) =>
     //return true IF ANY INPUT FIELD FOUND EMPTY
-    Object.values(inputs).includes("") &&
+    Object.values(inputs).includes("") ||
     // else CHECK FOR ERRORS IN INPUT FIELDS
     // if ANY ERROR FOUND return true
     Object.values(errors).findIndex((e) => e !== "") >= 0;
