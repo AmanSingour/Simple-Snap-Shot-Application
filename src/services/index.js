abd45b9a0ef24168bda769b3693d8a3b68730a01
Aman Singour <amansigour15@gmail.com>
2021-06-15 12:53:33 +0530

    Validation mathods exported

--- src/services/index.js
+++ src/services/index.js
@@ -1,2 +1,3 @@
 export {default as store} from './store'
-export {default as validate} from './form-validation/validate'
+export {default as validateInputs} from './form-validation/input.validate'
+export {default as validateForm} from './form-validation/form.validate'
